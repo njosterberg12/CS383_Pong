1479454370ae1b7b817b6e02042a9fa7ed24b1c4
njosterberg <njosterberg12@outlook.com>
2019-09-03 08:36:51 -0700

    Add empty project

---
+++ Menu/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame ()
+    {
+        // loads the next scene in the build queue which is Play
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void QuitGame()
+    {
+        // Quit to exit game; "Quit" is sent to the console instead of new scene to finish the application
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Menu/Assets/Paddle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float height;
+
+    string input; // for user determined input
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+        speed = 5f;
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+
+        isRight = isRightPaddle;
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle)
+        {
+            // paddle on right of screen
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; // adjusts paddles to be screen instead of on the end
+
+            input = "PaddleRight";
+        }
+        else
+        {
+            // paddle on left of screen
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
+        }
+
+        // updates paddle position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed; // adjusts paddle speed with relative framerate
+
+        transform.Translate(move * Vector2.up);
+
+        if (transform.position.y < GameManager.bottomLeft.y + height/2 && move < 0)
+        {
+            move = 0;
+        }
+
+        if (transform.position.y > GameManager.topRight.y + height/2 && move > 0)
+        {
+            move = 0;
+        }
+    }
+}
